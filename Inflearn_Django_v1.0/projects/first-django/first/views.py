a3e6572c99d4485dd1e91215ddf5208bfda56591
jysaa5 <jysaa5@naver.com>
2021-01-11 23:18:50 +0900

    Update views.py

--- Inflearn_Django_v1.0/projects/first-django/first/views.py
+++ Inflearn_Django_v1.0/projects/first-django/first/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    # HttpResponse: 클래스
     return HttpResponse("Hello World")
+
+
+def select(request):
+    message= '수 하나를 입력해주세요.'
+    return HttpResponse(message)
