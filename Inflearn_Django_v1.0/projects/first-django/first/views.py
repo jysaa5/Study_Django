114bc5b779a01cfa88fb5740cfe66cdbdcbcfa2c
jysaa5 <jysaa5@naver.com>
2021-01-13 00:20:28 +0900

    Study Django

--- Inflearn_Django_v1.0/projects/first-django/first/views.py
+++ Inflearn_Django_v1.0/projects/first-django/first/views.py
@@ -6,19 +6,25 @@ from datetime import datetime
 # Create your views here.
 def index(request):
     # HttpResponse: 클래스
-    template = loader.get_template('index.html')
+    # template = loader.get_template('index.html')
+    # 현재 시간
     now = datetime.now()
     context = {
         'current_date': now
     }
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
 
 
 def select(request):
-    message = '수 하나를 입력해주세요.'
-    return HttpResponse(message)
+    # message = '수 하나를 입력해주세요.'
+    context = {'number': 3}
+    # return HttpResponse(message)
+    return render(request, 'select.html', context)
 
 
 def result(request):
-    message = '추첨 결과입니다.'
-    return HttpResponse(message)
+    # message = '추첨 결과입니다.'
+    context = {'numbers': [1,2,3,4,5,6]}
+    # return HttpResponse(message)
+    return render(request, 'result.html', context)
