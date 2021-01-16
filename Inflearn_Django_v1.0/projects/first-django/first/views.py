663b6af4219e21fe86c541adf158cab8d1de5312
jysaa5 <jysaa5@naver.com>
2021-01-16 22:14:38 +0900

    Study Django

--- Inflearn_Django_v1.0/projects/first-django/first/views.py
+++ Inflearn_Django_v1.0/projects/first-django/first/views.py
@@ -13,18 +13,18 @@ def index(request):
         'current_date': now
     }
     # return HttpResponse(template.render(context, request))
-    return render(request, 'index.html', context)
+    return render(request, 'first/index.html', context)
 
 
 def select(request):
     # message = '수 하나를 입력해주세요.'
     context = {'number': 3}
     # return HttpResponse(message)
-    return render(request, 'select.html', context)
+    return render(request, 'first/select.html', context)
 
 
 def result(request):
     # message = '추첨 결과입니다.'
     context = {'numbers': [1,2,3,4,5,6]}
     # return HttpResponse(message)
-    return render(request, 'result.html', context)
+    return render(request, 'first/result.html', context)
