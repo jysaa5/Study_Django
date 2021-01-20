1bfcb21c579d9f2a78e9d4f4f5441876badfc415
jysaa5 <jysaa5@naver.com>
2021-01-21 00:28:19 +0900

    Study Django

--- Inflearn_Django_v1.0/projects/first-django/second/urls.py
+++ Inflearn_Django_v1.0/projects/first-django/second/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('list/', views.list, name="list"),
+    path('create/', views.create, name="create"),
 
 ]
--- Inflearn_Django_v1.0/projects/first-django/second/views.py
+++ Inflearn_Django_v1.0/projects/first-django/second/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from second.models import Post
+from .forms import PostForm
 
 # Create your views here.
+def list(request):
+    context = {
+        'items': Post.objects.all()
+    }
+    return render(request, 'second/list.html', context)
+
+
+def create(request):
+    form = PostForm()
+    return render(request, 'second/create.html', {'form': form})
